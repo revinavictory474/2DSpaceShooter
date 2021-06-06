657a423bafc4de50cfdb8e6ea0e0272b66a347d6
Revina Victory <revina.victory@gmail.com>
2021-06-06 21:58:03 +0700

    player move on board

--- 2D Space Shooter/Assets/Code/PlayerMove.cs
+++ 2D Space Shooter/Assets/Code/PlayerMove.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static PlayerMove instance;
+    public Borders borders;
+    private Camera _camera;
+    public int speedPlayer;
+    private Vector2 _mousePosition;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+            instance = this;
+        else Destroy(gameObject);
+        _camera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        ResizeBorders();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            _mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = Vector2.MoveTowards(transform.position, _mousePosition, speedPlayer * Time.deltaTime); 
+        }
+
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, borders.minX, borders.maxX), Mathf.Clamp(transform.position.y, borders.minY, borders.maxY)); 
+    }
+
+    private void ResizeBorders()
+    {
+        borders.minX = _camera.ViewportToWorldPoint(Vector2.zero).x + borders.minXOffset;
+        borders.minY = _camera.ViewportToWorldPoint(Vector2.zero).y + borders.minYOffset;
+        borders.maxX = _camera.ViewportToWorldPoint(Vector2.right).x - borders.maxXOffset;
+        borders.maxY = _camera.ViewportToWorldPoint(Vector2.up).y - borders.maxYOffset;
     }
 }
