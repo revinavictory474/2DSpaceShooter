5c9c44447496dbda35372904c2ee46c894436c54
Revina Victory <revina.victory@gmail.com>
2021-06-07 16:30:14 +0700

    wave settings, version 0.1

---
+++ 2D Space Shooter/Assets/Code/LevelController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LevelController : MonoBehaviour
+{
+    public static LevelController instance;
+    public GameObject[] playerShips;
+    public EnemyWaves[] enemyWaves;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        for ( int i = 0; i< enemyWaves.Length; i++)
+        {
+            StartCoroutine(CreateEnemyWave(enemyWaves[i].timeToStart,enemyWaves[i].wave));
+        }
+    }
+
+    private IEnumerator CreateEnemyWave(float delay, GameObject wave)
+    {
+        if (delay != 0)
+            yield return new WaitForSeconds(delay);
+        if (Player.instance != null)
+            Instantiate(wave); 
+    }
+}
