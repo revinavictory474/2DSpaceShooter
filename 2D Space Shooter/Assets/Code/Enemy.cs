5d018e337f55318eb4bd948e4dda761a3683cb44
Revina Victory <revina.victory@gmail.com>
2021-06-07 13:17:01 +0700

    waypoint enemy, destroy enemy ontriggerenter

---
+++ 2D Space Shooter/Assets/Code/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public int enemeHealth;
+
+   
+
+    public void GetDamage(int damage)
+    {
+        enemeHealth -= damage;
+
+        if(enemeHealth <=0)
+        {
+            Destruction();
+        }
+    }
+
+    private void Destruction()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            GetDamage(1);
+        }
+    }
+}
---
+++ 2D Space Shooter/Assets/Code/FollowThePath.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowThePath : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public float speedEnemy;
+    public bool isReturn;
+    public Vector3[] newPosition;
+    private int currentPosition;
+
+    private void Start()
+    {
+        newPosition = NewPosition(waypoints);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, newPosition[currentPosition], speedEnemy * Time.deltaTime);
+        if(Vector3.Distance(transform.position, newPosition[currentPosition]) < 0.2f)
+        {
+            currentPosition++;
+            if(isReturn&&Vector3.Distance(transform.position, newPosition[newPosition.Length -1]) < 0.3f)
+            {
+                currentPosition = 0;
+            }
+        }
+
+        if (Vector3.Distance(transform.position, newPosition[newPosition.Length - 1]) < 0.2f && !isReturn)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private Vector3[] NewPosition(Transform[] pathPosition)
+    {
+        Vector3[] pathPosit = new Vector3[pathPosition.Length];
+
+        for( int i =0; i < waypoints.Length; i++)
+        {
+            pathPosit[i] = pathPosition[i].position;
+        }
+        return pathPosit;
+
+    }
+
+
+}
